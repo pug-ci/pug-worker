27cb6a95af57b4c8a5002939c9f2361e60b19d1e
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-12-21 00:16:27 +0100

    Add basic shell builder helper methods

--- lib/pug/worker/script/shell/builder.rb
+++ lib/pug/worker/script/shell/builder.rb
@@ -3,6 +3,22 @@ module Pug
     module Script
       module Shell
         class Builder
+          def initialize
+            @cmds = []
+          end
+
+          def compile
+            # TODO: shellwords
+            @cmds.join "\n"
+          end
+
+          def cmd(data)
+            @cmds << data
+          end
+
+          def cd(data)
+            @cmds << "cd #{data}"
+          end
         end
       end
     end
